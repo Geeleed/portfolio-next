9849a365e2ecf728f944cead9794051f00288861
Geeleed <surasak.kaewpho@gmail.com>
2023-09-24 12:11:31 +0700

    แก้ page.js

--- src/app/components/miniApps/page.js
+++ src/app/components/miniApps/page.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import Glassmorphism from './Glassmorphism'
-import '../../CSS.css'
-import Glassmorphism0 from './Glassmorphism0'
-import Snack from './Snack'
-import RateExchange from './RateExchange'
-
-function Page() {
-  return (
-    <div>
-        {/* <Glassmorphism/> */}
-        {/* <br/> */}
-        {/* <Glassmorphism0/> */}
-        {/* <Snack/> */}
-        {/* <br/> */}
-        {/* <RateExchange/> */}
-        {/* <br/> */}
-    </div>
-  )
-}
-
-export default Page
-
 // import React from 'react'
+// import Glassmorphism from './Glassmorphism'
+// import '../../CSS.css'
+// import Glassmorphism0 from './Glassmorphism0'
+// import Snack from './Snack'
+// import RateExchange from './RateExchange'
 
 // function Page() {
 //   return (
-//     <div>page</div>
+//     <div>
+//         {/* <Glassmorphism/> */}
+//         {/* <br/> */}
+//         {/* <Glassmorphism0/> */}
+//         {/* <Snack/> */}
+//         {/* <br/> */}
+//         {/* <RateExchange/> */}
+//         {/* <br/> */}
+//     </div>
 //   )
 // }
 
 // export default Page
+
+import React from 'react'
+
+function Page() {
+  return (
+    <div>page in miniApps</div>
+  )
+}
+
+export default Page
