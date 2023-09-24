032d791bbe70db8140d4f8a55c1a63b9f728ca05
Geeleed <surasak.kaewpho@gmail.com>
2023-09-24 10:53:18 +0700

    อันล่าสุด deploy error

--- src/app/components/miniApps/page.js
+++ src/app/components/miniApps/page.js
@@ -1,21 +1,31 @@
+// import React from 'react'
+// import Glassmorphism from './Glassmorphism'
+// import '../../CSS.css'
+// import Glassmorphism0 from './Glassmorphism0'
+// import Snack from './Snack'
+// import RateExchange from './RateExchange'
+
+// function page() {
+//   return (
+//     <div>
+//         {/* <Glassmorphism/> */}
+//         <br/>
+//         {/* <Glassmorphism0/> */}
+//         {/* <Snack/> */}
+//         <br/>
+//         {/* <RateExchange/> */}
+//         <br/>
+//     </div>
+//   )
+// }
+
+// export default page
+
 import React from 'react'
-import Glassmorphism from './Glassmorphism'
-import '../../CSS.css'
-import Glassmorphism0 from './Glassmorphism0'
-import Snack from './Snack'
-import RateExchange from './RateExchange'
 
 function page() {
   return (
-    <div>
-        {/* <Glassmorphism/> */}
-        <br/>
-        {/* <Glassmorphism0/> */}
-        {/* <Snack/> */}
-        <br/>
-        {/* <RateExchange/> */}
-        <br/>
-    </div>
+    <div>page</div>
   )
 }
 
