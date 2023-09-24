b5a60a8aeddff3a16b7f13816f02c54e46306575
Geeleed <surasak.kaewpho@gmail.com>
2023-09-24 13:22:43 +0700

    เพิ่ม page.js แต่ไม่ใส่ <Glassmorphism/>

---
+++ src/app/components/miniApps/page.js
@@ -0,0 +1,16 @@
+'use client'
+import React from 'react'
+import '../../CSS.css'
+import Snack from './Snack'
+import RateExchange from './RateExchange'
+
+function MiniAppsPage() {
+  return (
+    <div >
+      <Snack /><br />
+      <RateExchange /><br />
+    </div>
+  )
+}
+
+export default MiniAppsPage
