d1adfbb73a91bcde3210cfb30ed999a593a6dd98
Geeleed <surasak.kaewpho@gmail.com>
2023-09-24 11:25:28 +0700

    เปิด page.js

---
+++ src/app/components/miniApps/page.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Glassmorphism from './Glassmorphism'
+import '../../CSS.css'
+import Glassmorphism0 from './Glassmorphism0'
+import Snack from './Snack'
+import RateExchange from './RateExchange'
+
+function page() {
+  return (
+    <div>
+        <Glassmorphism/>
+        <br/>
+        {/* <Glassmorphism0/> */}
+        <Snack/>
+        <br/>
+        <RateExchange/>
+        <br/>
+    </div>
+  )
+}
+
+export default page
+
+// import React from 'react'
+
+// function Page() {
+//   return (
+//     <div>page</div>
+//   )
+// }
+
+// export default Page
