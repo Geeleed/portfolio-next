d63b95e81e01a847e479c5f2da7f5d716c2330d1
Geeleed <surasak.kaewpho@gmail.com>
2023-10-08 09:11:50 +0700

    หา bug

--- src/app/components/Profile.js
+++ src/app/components/Profile.js
@@ -1,45 +1,35 @@
-// import React from 'react'
-// import css from './Profile.module.css'
-// import Image from 'next/image'
-// import Link from 'next/link'
-
-// function Profile() {
-//   return (
-//     <div id='profile' className={css.main}>
-//       <div className={css.disk}>
-//         <Image src='/images/profile/1.jpg' width={700} height={700} alt='' />
-//         <div></div>
-//       </div>
-//       <div className={css.text}>
-//         <h1>SURASAK KAEWPHO</h1>
-//         <p>จบฟิสิกส์และต้องการทำงานด้าน Web Developer มีทักษะคอมพิวเตอร์เบื้องต้น ใช้ภาษา HTML CSS JavaScript React(Next.JS) ได้เบื้องต้น สามารถเชื่อมต่อจัดการข้อมูลใน database และใช้งานหรือพัฒนา API ด้วย Express/Python ในระดับเบื้องต้นได้ สามารถติดต่อได้ที่ <Link href={'mailto:surasak.kwork@gmail.com'}>surasak.kwork@gmail.com</Link> หรือ social media ที่ส่วน Contact ขอบคุณครับ</p>
-//         <div>
-//           <Image src={'/images/logo/pngwing.com (16).png'} width={42} height={47} alt=''/>
-//           <Image src={'/images/logo/pngwing.com (17).png'} width={52} height={50} alt=''/>
-//           <Image src={'/images/logo/pngwing.com (18).png'} width={50} height={50} alt=''/>
-//           <Image src={'/images/logo/pngwing.com (14).png'} width={50} height={50} alt=''/>
-//           <Image src={'/images/logo/pngwing.com (15).png'} width={50} height={50} alt=''/>
-//           <Image src={'/images/logo/pngwing.com (3).png'} width={50} height={50} alt=''/>
-//           <Image style={{filter:'invert(1)'}} src={'/images/logo/pngwing.com (7).png'} width={60} height={50} alt=''/>
-//           <Image src={'/images/logo/pngwing.com (9).png'} width={50} height={50} alt=''/>
-//           <Image src={'/images/logo/pngwing.com (11).png'} width={60} height={50} alt=''/>
-//           <Image src={'/images/logo/pngwing.com (19).png'} width={50} height={50} alt=''/>
-//           <Image style={{filter:'invert(1)'}} src={'/images/logo/512x512-logo-27148.png'} width={50} height={50} alt=''/>
-//           <Image src={'/images/logo/pngwing.com.png'} width={50} height={50} alt=''/>
-//           <Image src={'/images/logo/pngwing.com (10).png'} width={50} height={50} alt=''/>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Profile
-
 import React from 'react'
+import css from './Profile.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <div id='profile' className={css.main}>
+      <div className={css.disk}>
+        <Image src='/images/profile/1.jpg' width={700} height={700} alt='' />
+        <div></div>
+      </div>
+      <div className={css.text}>
+        <h1>SURASAK KAEWPHO</h1>
+        <p>จบฟิสิกส์และต้องการทำงานด้าน Web Developer มีทักษะคอมพิวเตอร์เบื้องต้น ใช้ภาษา HTML CSS JavaScript React(Next.JS) ได้เบื้องต้น สามารถเชื่อมต่อจัดการข้อมูลใน database และใช้งานหรือพัฒนา API ด้วย Express/Python ในระดับเบื้องต้นได้ สามารถติดต่อได้ที่ <Link href={'mailto:surasak.kwork@gmail.com'}>surasak.kwork@gmail.com</Link> หรือ social media ที่ส่วน Contact ขอบคุณครับ</p>
+        <div>
+          <Image src={'/images/logo/pngwing.com (16).png'} width={42} height={47} alt=''/>
+          <Image src={'/images/logo/pngwing.com (17).png'} width={52} height={50} alt=''/>
+          <Image src={'/images/logo/pngwing.com (18).png'} width={50} height={50} alt=''/>
+          <Image src={'/images/logo/pngwing.com (14).png'} width={50} height={50} alt=''/>
+          <Image src={'/images/logo/pngwing.com (15).png'} width={50} height={50} alt=''/>
+          <Image src={'/images/logo/pngwing.com (3).png'} width={50} height={50} alt=''/>
+          <Image style={{filter:'invert(1)'}} src={'/images/logo/pngwing.com (7).png'} width={60} height={50} alt=''/>
+          <Image src={'/images/logo/pngwing.com (9).png'} width={50} height={50} alt=''/>
+          <Image src={'/images/logo/pngwing.com (11).png'} width={60} height={50} alt=''/>
+          <Image src={'/images/logo/pngwing.com (19).png'} width={50} height={50} alt=''/>
+          <Image style={{filter:'invert(1)'}} src={'/images/logo/512x512-logo-27148.png'} width={50} height={50} alt=''/>
+          <Image src={'/images/logo/pngwing.com.png'} width={50} height={50} alt=''/>
+          <Image src={'/images/logo/pngwing.com (10).png'} width={50} height={50} alt=''/>
+        </div>
+      </div>
+    </div>
   )
 }
 
