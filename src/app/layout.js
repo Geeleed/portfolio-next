91787c0035853aa1e3be9bccb964091701f922c4
Geeleed <surasak.kaewpho@gmail.com>
2023-10-08 09:14:29 +0700

    หา bug

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,9 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
+      {/* <head>
         <link rel='icon' href='svg/myicon.svg' />
-      </head>
+      </head> */}
       <body className={inter.className}>{children}</body>
     </html>
   )
