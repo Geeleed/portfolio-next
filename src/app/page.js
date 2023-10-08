82e61f1f760a358e164f258cf2c2d9688187c60d
Geeleed <surasak.kaewpho@gmail.com>
2023-10-08 08:56:27 +0700

    หา bug

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import page_style from './page.module.css'
-import Image from 'next/image'
-import ShowUI from './reuses/ShowUI'
-import PhotoEditor from './components/PhotoEditor'
-import styled from 'styled-components'
+// import page_style from './page.module.css'
+// import Image from 'next/image'
+// import ShowUI from './reuses/ShowUI'
+// import PhotoEditor from './components/PhotoEditor'
+// import styled from 'styled-components'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Profile from './components/Profile'
-import Head from 'next/head'
+// import Head from 'next/head'
 import './globals.css'
 import './CSS.css'
 
@@ -18,8 +18,8 @@ import ContactBar from './components/ContactBar'
 import LoadingPage from './components/LoadingPage'
 // import MiniAppsPage from './components/miniApps/MiniAppsPage'
 // import Glassmorphism from './components/miniApps/Glassmorphism'
-import Snack from './components/miniApps/Snack'
-import RateExchange from './components/miniApps/RateExchange'
+// import Snack from './components/miniApps/Snack'
+// import RateExchange from './components/miniApps/RateExchange'
 // import Glassmorphism from './components/miniApps/Glassmorphism'
 
 function Page() {
@@ -34,7 +34,7 @@ function Page() {
         <Home />
         <Profile />
         <Portfolio />
-        {/* <Contact /> */}
+        <Contact />
         <ContactBar />
         {/* <MiniAppsPage/> */}
       </div>
